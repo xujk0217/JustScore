a4a011b36aa0cbc3bce096150eb79f0820e7c4ae
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-11-17 00:29:44 +0800

    Initial Commit

---
+++ JustScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustScore
+//
+//  Created by 許君愷 on 2024/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustScore/JustScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustScoreApp.swift
+//  JustScore
+//
+//  Created by 許君愷 on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct JustScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
